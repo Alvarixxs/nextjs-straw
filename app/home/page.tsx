b481f18ddd052d100271ab17c9163e5591e7d6db
alvarixxs <alvarodesants@gmail.com>
2024-06-24 17:53:49 +0200

    solved dependancies

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,6 @@ import Home from "@/app/ui/home/Home";
 import Intro from "@/app/ui/home/Intro";
 import Questions from "@/app/ui/home/Questions";
 import Reviews from "@/app/ui/home/Reviews";
-import Starfield from "react-starfield";
 
 export default function Page() {
   return (
@@ -11,7 +10,6 @@ export default function Page() {
       <Intro />
       <Questions />
       <Reviews />
-
     </main>
   );
 }
